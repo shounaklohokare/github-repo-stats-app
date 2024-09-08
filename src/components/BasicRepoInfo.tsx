c163cdfae3688bd7c8cf5b1f1ccd530df9488046
Shounak Lohokare <shounaklohokare@gmail.com>
2024-09-08 19:27:09 +0530

    added mobile friendly layout to repo stats component

--- src/components/BasicRepoInfo.tsx
+++ src/components/BasicRepoInfo.tsx
@@ -31,26 +31,7 @@ const BasicRepoInfo:FC<BasicRepoInfoProps> = ({ basicRepoInfo, userData  }) => {
     const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
 
 
-    return (userData.id && <div className="ml-6 flex my-8">
-                                <div className="flex p-2 md:flex-row flex-col md:space-x-8 mt-1 md:mx-8">
-                                    <RepoStat Icon={GoRepoForked} property="Forks" value={basicRepoInfo?.forks} />
-                                    <RepoStat Icon={GoIssueOpened} property="Open Issues" value={basicRepoInfo?.open_issues} />
-                                    <RepoStat Icon={GoStar} property="Stars" value={basicRepoInfo?.watchers_count} />
-                                    <RepoStat Icon={GoGitBranch} property="Default Branch" value={basicRepoInfo?.default_branch} />
-                                </div>
-                                <div className="flex p-2  md:flex-row flex-col font-mono">
-                                        <img src={userData?.avatar_url} className="md:h-[5.83rem] md:w-[5.83rem] h-[4.23rem] w-[4.23rem] rounded-md"/>
-                                        <div className="flex flex-col md:pl-6 mt-4">
-                                            <span>{basicRepoInfo?.owner?.login}</span>
-                                            <span>{basicRepoInfo?.name}</span>
-                                        </div>
-                                </div>
-                                <div className="flex p-2 md:flex-row flex-col md:space-x-8 mt-1 md:mx-8">
-                                    <RepoStat Icon={GoEye} property="Watchers" value={basicRepoInfo.subscribers_count} />
-                                    <RepoStat Icon={MdDateRange} property="Created" value={(isMobile) ? getShortDate(basicRepoInfo?.created_at)  : getFormattedDate(basicRepoInfo?.created_at)} />
-                                    <RepoStat Icon={MdDateRange} property="Updated" value={(isMobile) ? getShortDate(basicRepoInfo?.updated_at)  : getFormattedDate(basicRepoInfo?.updated_at)} />
-                                </div>
-                            </div>)
+    return (userData.id && (isMobile ? <InfoPanelMobile basicRepoInfo={basicRepoInfo} userData={userData}/> : <InfoPanelDesktop basicRepoInfo={basicRepoInfo} userData={userData}/>))
 
 }
 
@@ -60,10 +41,63 @@ interface RepoStatProps{
     value: string;
 }
 
+const InfoPanelDesktop:FC<BasicRepoInfoProps> = ({ basicRepoInfo, userData  }) => {
+
+    return (<div className="ml-6 flex my-8">
+            <div className="flex p-2 flex-row space-x-8 mt-1 mx-8">
+                <RepoStat Icon={GoRepoForked} property="Forks" value={basicRepoInfo?.forks} />
+                <RepoStat Icon={GoIssueOpened} property="Open Issues" value={basicRepoInfo?.open_issues} />
+                <RepoStat Icon={GoStar} property="Stars" value={basicRepoInfo?.watchers_count} />
+                <RepoStat Icon={GoGitBranch} property="Default Branch" value={basicRepoInfo?.default_branch} />
+            </div>
+            <div className="flex p-2 flex-row font-mono">
+                    <img src={userData?.avatar_url} className="md:h-[5.83rem] md:w-[5.83rem] h-[4.23rem] w-[4.23rem] rounded-md"/>
+                    <div className="flex flex-col md:pl-6 mt-4">
+                        <span>{basicRepoInfo?.owner?.login}</span>
+                        <span>{basicRepoInfo?.name}</span>
+                    </div>
+            </div>
+            <div className="flex p-2 flex-row md:space-x-8 mt-1 mx-8">
+                <RepoStat Icon={GoEye} property="Watchers" value={basicRepoInfo.subscribers_count} />
+                <RepoStat Icon={MdDateRange} property="Created" value={getFormattedDate(basicRepoInfo?.created_at)} />
+                <RepoStat Icon={MdDateRange} property="Updated" value={getFormattedDate(basicRepoInfo?.updated_at)} />
+            </div>
+        </div>)
+
+}
+
+const InfoPanelMobile:FC<BasicRepoInfoProps> = ({ basicRepoInfo, userData  }) => {
+
+    return (<div className="flex flex-col mt-8 mb-12">
+                 <div className="flex w-full p-2 items-center  flex-col font-mono">
+                    <img src={userData?.avatar_url} className="md:h-[5.83rem] md:w-[5.83rem] h-[4.23rem] w-[4.23rem] rounded-md"/>
+                    <div className="flex flex-col items-center md:pl-6 mt-4">
+                        <span>{basicRepoInfo?.owner?.login}</span>
+                        <span>{basicRepoInfo?.name}</span>
+                    </div>
+                </div>
+                <div className="flex w-full">
+                    <div className="flex w-1/2 flex-col">
+                        <RepoStat Icon={GoRepoForked} property="Forks" value={basicRepoInfo?.forks} />
+                        <RepoStat Icon={GoIssueOpened} property="Open Issues" value={basicRepoInfo?.open_issues} />
+                        <RepoStat Icon={GoStar} property="Stars" value={basicRepoInfo?.watchers_count} />
+                        <RepoStat Icon={GoGitBranch} property="Default Branch" value={basicRepoInfo?.default_branch} />
+                    </div>
+                    <div className="flex w-1/2  flex-col">
+                        <RepoStat Icon={GoEye} property="Watchers" value={basicRepoInfo.subscribers_count} />
+                        <RepoStat Icon={MdDateRange} property="Created" value={getShortDate(basicRepoInfo?.created_at)} />
+                        <RepoStat Icon={MdDateRange} property="Updated" value={getShortDate(basicRepoInfo?.updated_at)} />
+                    </div>
+                </div>
+            </div>)
+
+
+
+}
 
 const RepoStat:FC<RepoStatProps> = ({Icon, property, value}) => {
 
-    return <div className="flex space-x-3 pt-7 font-mono">
+    return <div className="flex space-x-3 pt-7 ml-7 font-mono text-nowrap">
                 <Icon size="24"/>
                 <span>{property}</span>
                 <span>{value}</span>
